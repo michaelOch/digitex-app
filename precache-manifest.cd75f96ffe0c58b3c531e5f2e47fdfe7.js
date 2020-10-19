90737a934c09262a46534d9d6177d2c93525b3bb
michaelOch <ochuenwikechukwudi@hotmail.com>
2020-10-19 09:42:29 -0700

    Updates

---
+++ precache-manifest.cd75f96ffe0c58b3c531e5f2e47fdfe7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a07ed799547704d28a87382400f658d",
+    "url": "/digitex-app/index.html"
+  },
+  {
+    "revision": "6a0253238853b0007d9b",
+    "url": "/digitex-app/static/css/2.6d4ecc00.chunk.css"
+  },
+  {
+    "revision": "7b39b29b7e7ecd4c91f7",
+    "url": "/digitex-app/static/css/main.22749773.chunk.css"
+  },
+  {
+    "revision": "6a0253238853b0007d9b",
+    "url": "/digitex-app/static/js/2.ad18c16b.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/digitex-app/static/js/2.ad18c16b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b39b29b7e7ecd4c91f7",
+    "url": "/digitex-app/static/js/main.b395b979.chunk.js"
+  },
+  {
+    "revision": "9757b283fac8c9d87a33",
+    "url": "/digitex-app/static/js/runtime-main.116fcc2d.js"
+  }
+]);
