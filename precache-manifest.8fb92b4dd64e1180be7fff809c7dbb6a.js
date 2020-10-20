3a7731a921c0352281c5e38aee658e6645fea6da
michaelOch <ochuenwikechukwudi@hotmail.com>
2020-10-20 13:14:33 -0700

    Updates

---
+++ precache-manifest.8fb92b4dd64e1180be7fff809c7dbb6a.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acf3e4ff9d79f5fc82e6d86dc9fef5f1",
+    "url": "/digitex-app/index.html"
+  },
+  {
+    "revision": "e9626b926964481acc25",
+    "url": "/digitex-app/static/css/2.6d4ecc00.chunk.css"
+  },
+  {
+    "revision": "a9524060edbaa8b5f6e4",
+    "url": "/digitex-app/static/css/main.7911f13d.chunk.css"
+  },
+  {
+    "revision": "e9626b926964481acc25",
+    "url": "/digitex-app/static/js/2.218ad6f0.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/digitex-app/static/js/2.218ad6f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9524060edbaa8b5f6e4",
+    "url": "/digitex-app/static/js/main.a49b60b6.chunk.js"
+  },
+  {
+    "revision": "9757b283fac8c9d87a33",
+    "url": "/digitex-app/static/js/runtime-main.116fcc2d.js"
+  },
+  {
+    "revision": "16edda61d5b6ceba4f4b3cac4c658ca0",
+    "url": "/digitex-app/static/media/changelly.16edda61.webp"
+  },
+  {
+    "revision": "310d568657a071f7ac2c3764fa8c620f",
+    "url": "/digitex-app/static/media/coindcx.310d5686.webp"
+  },
+  {
+    "revision": "7880be983705e51058c7def636b43d30",
+    "url": "/digitex-app/static/media/coinex.7880be98.webp"
+  },
+  {
+    "revision": "d06a43236e302967a6571dc57d1e7709",
+    "url": "/digitex-app/static/media/etherium.d06a4323.webp"
+  },
+  {
+    "revision": "18bac467f3ad7d0ec0a3b2864c1ca2b5",
+    "url": "/digitex-app/static/media/fatbtc.18bac467.webp"
+  },
+  {
+    "revision": "bc47ece2dbafa1530f5de13e268b1960",
+    "url": "/digitex-app/static/media/hitbtc.bc47ece2.webp"
+  },
+  {
+    "revision": "085fd6eeff196ba7b31cd24b8b6968db",
+    "url": "/digitex-app/static/media/kucoin.085fd6ee.webp"
+  },
+  {
+    "revision": "7a8bd0a9afe0194a69b78f4b0252f321",
+    "url": "/digitex-app/static/media/logo.7a8bd0a9.svg"
+  },
+  {
+    "revision": "bc6c88d2f9c794ab4cbe2ad35ae1a569",
+    "url": "/digitex-app/static/media/mercatox.bc6c88d2.webp"
+  },
+  {
+    "revision": "a9bd072c46d2f9f0268a92c5c8f5aa39",
+    "url": "/digitex-app/static/media/probit.a9bd072c.webp"
+  },
+  {
+    "revision": "686a18749313d5b228328dcecba20e35",
+    "url": "/digitex-app/static/media/sale-icon-2.686a1874.svg"
+  },
+  {
+    "revision": "747e8549999ab9807d02ffb20396af8e",
+    "url": "/digitex-app/static/media/sale-icon-3.747e8549.svg"
+  },
+  {
+    "revision": "41ba474b63301438183e4a6b4ea59bb3",
+    "url": "/digitex-app/static/media/sale-icon-4.41ba474b.svg"
+  },
+  {
+    "revision": "78b643fa8f9b1d8405d05288952ec49d",
+    "url": "/digitex-app/static/media/sale-icon.78b643fa.svg"
+  },
+  {
+    "revision": "790a142faa4bed7372c9fb84db06aa61",
+    "url": "/digitex-app/static/media/vcc-exchange.790a142f.webp"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/digitex-app/precache-manifest.a900e9298d85e9633913c2f414a8bf95.js"
+  "/digitex-app/precache-manifest.8fb92b4dd64e1180be7fff809c7dbb6a.js"
 );
 
 self.addEventListener('message', (event) => {
